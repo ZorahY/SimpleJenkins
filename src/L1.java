7cd4f6886424a9e2da3d9c7282680df62afac93a
ZorahY <zorah.yavaveva@gmail.com>
2023-03-22 21:02:46 -0400

    first commit

---
+++ src/L1.java
@@ -0,0 +1,10 @@
+public class L1 {
+    public static void main(String[] args) throws InterruptedException {
+       for (int i=0;i<10; i++){
+           System.out.println("Hello World");
+           Thread.sleep(4000);
+       }
+    }
+
+
+}
